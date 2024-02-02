961e50bb88d285af6fa2774bda48d3d75c6ea07a
Dan Pham <phamdan.sj@gmail.com>
2024-02-01 21:45:43 -0800

    Initial Commit

---
+++ Fetch iOS Coding Challenge/Fetch_iOS_Coding_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_iOS_Coding_ChallengeApp.swift
+//  Fetch iOS Coding Challenge
+//
+//  Created by Dan Pham on 2/1/24.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_iOS_Coding_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
