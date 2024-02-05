b0f9a3f7decdebd278d135d104ddd3d6fd968994
Dan Pham <phamdan.sj@gmail.com>
2024-02-05 13:37:16 -0800

    Refactor naming from Dessert to Meal to match business requirements langauge

--- Fetch iOS Coding Challenge/MealsListView.swift
+++ Fetch iOS Coding Challenge/MealsListView.swift
@@ -1,5 +1,5 @@
 //
-//  DessertListView.swift
+//  MealsListView.swift
 //  Fetch iOS Coding Challenge
 //
 //  Created by Dan Pham on 2/1/24.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct DessertItem: Identifiable {
+struct Meal: Identifiable {
     let id: String
     let title: String
     let thumbnailURL: String
@@ -19,15 +19,15 @@ struct DessertItem: Identifiable {
     }
 }
 
-struct DessertListView: View {
+struct MealsListView: View {
     
     @State private var viewModel = ViewModel()
     
     var body: some View {
         NavigationStack {
             List {
-                ForEach(viewModel.dessertItems) { dessertItem in
-                    DessertListCellView(dessertItem: dessertItem)
+                ForEach(viewModel.meals) { meal in
+                    MealListCellView(meal: meal)
                 }
                 .listRowSeparator(.hidden)
             }
@@ -38,30 +38,30 @@ struct DessertListView: View {
 }
 
 #Preview {
-    DessertListView()
+    MealsListView()
 }
 
 
-extension DessertListView {
+extension MealsListView {
     @Observable
     class ViewModel {
-        let dessertItems: [DessertItem] = [
-            DessertItem(id: "52893", title: "Apple & Blackberry Crumble", thumbnailURL: "https://www.themealdb.com/images/media/meals/xvsurr1511719182.jpg"),
-            DessertItem(id: "52768", title: "Apple Frangipan Tart", thumbnailURL: "https://www.themealdb.com/images/media/meals/wxywrq1468235067.jpg"),
-            DessertItem(id: "52855", title: "Banana Pancakes", thumbnailURL: "https://www.themealdb.com/images/media/meals/sywswr1511383814.jpg")
+        let meals: [Meal] = [
+            Meal(id: "52893", title: "Apple & Blackberry Crumble", thumbnailURL: "https://www.themealdb.com/images/media/meals/xvsurr1511719182.jpg"),
+            Meal(id: "52768", title: "Apple Frangipan Tart", thumbnailURL: "https://www.themealdb.com/images/media/meals/wxywrq1468235067.jpg"),
+            Meal(id: "52855", title: "Banana Pancakes", thumbnailURL: "https://www.themealdb.com/images/media/meals/sywswr1511383814.jpg")
         ]
     }
 }
 
-struct DessertListCellView: View {
-    let dessertItem: DessertItem
+struct MealListCellView: View {
+    let meal: Meal
     
     var body: some View {
         VStack(alignment: .leading) {
             HStack(spacing: 16) {
-                DessertItemThumbnail(thumbnailURL: dessertItem.thumbnailURL)
+                MealThumbnail(thumbnailURL: meal.thumbnailURL)
                 
-                Text(dessertItem.title)
+                Text(meal.title)
                     .fontWeight(.medium)
                 
                 Spacer()
@@ -74,7 +74,7 @@ struct DessertListCellView: View {
     }
 }
 
-struct DessertItemThumbnail: View {
+struct MealThumbnail: View {
     let thumbnailURL: String
     
     var body: some View {
