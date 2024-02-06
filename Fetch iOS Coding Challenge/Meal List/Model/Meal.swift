85ffd919de6f40f585027f77f6dd4043a100bacc
Dan Pham <phamdan.sj@gmail.com>
2024-02-06 14:57:45 -0800

    Extract 'Meal' model protocols into separate extensions

--- Fetch iOS Coding Challenge/Meal List/Model/Meal.swift
+++ Fetch iOS Coding Challenge/Meal List/Model/Meal.swift
@@ -9,17 +9,21 @@ struct Meals: Decodable {
     var meals: [Meal]
 }
 
-struct Meal: Identifiable, Decodable, Comparable {
+struct Meal: Identifiable {
     var id: String
     var title: String
     var thumbnailURL: String
-    
+}
+
+extension Meal: Decodable {
     enum CodingKeys: String, CodingKey {
         case id = "idMeal"
         case title = "strMeal"
         case thumbnailURL = "strMealThumb"
     }
-    
+}
+
+extension Meal: Comparable {
     static func < (lhs: Meal, rhs: Meal) -> Bool {
         lhs.title < rhs.title
     }
